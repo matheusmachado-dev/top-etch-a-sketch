3b23dbd36bc103afef65378bef36c5133abf5f5b
matheusmachado-dev <matheusmc.dev@gmail.com>
2025-07-18 21:16:41 -0300

    feat: add `createGrid` function

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const container = document.querySelector(".container");
-console.log(container);
+CONTAINER_SIZE = 960;
+function createGrid(gridSize = 16) {
+  const container = document.querySelector(".container");
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridCell = document.createElement("div");
+    gridCell.style.width = `${CONTAINER_SIZE / gridSize}px`;
+    gridCell.style.height = `${CONTAINER_SIZE / gridSize}px`;
+    gridCell.classList.add("gridCell");
+    container.appendChild(gridCell);
+  }
+  return;
+}
+
+createGrid(16);
