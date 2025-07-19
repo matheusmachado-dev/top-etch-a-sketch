f3ae69458ccddadccb14bdff59402535eb6c6253
matheusmachado-dev <matheusmc.dev@gmail.com>
2025-07-18 22:35:51 -0300

    feat: add `btn-resize`

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ let gridSize = 16;
 
 const container = document.querySelector(".container");
 const resetBtn = document.querySelector(".btn-reset");
+const resizeBtn = document.querySelector(".btn-resize");
 
-resetBtn.addEventListener("click", handleResetButton);
+resetBtn.addEventListener("click", handleResetBtn);
+resizeBtn.addEventListener("click", handleResizeBtn);
 
 createGrid(gridSize);
 
-function createGrid(gridSize) {
-  for (let i = 0; i < gridSize * gridSize; i++) {
+function createGrid(newGridSize) {
+  gridSize = newGridSize;
+  for (let i = 0; i < newGridSize * newGridSize; i++) {
     const gridCell = document.createElement("div");
-    gridCell.style.width = `${CONTAINER_SIZE / gridSize}px`;
-    gridCell.style.height = `${CONTAINER_SIZE / gridSize}px`;
+    gridCell.style.width = `${CONTAINER_SIZE / newGridSize}px`;
+    gridCell.style.height = `${CONTAINER_SIZE / newGridSize}px`;
     gridCell.classList.add("gridCell");
     gridCell.addEventListener("mouseenter", () => {
       gridCell.style.backgroundColor = "blue";
@@ -21,7 +24,15 @@ function createGrid(gridSize) {
   }
   return;
 }
-function handleResetButton() {
+function removeGrid() {
   container.replaceChildren();
+}
+function handleResetBtn() {
+  removeGrid();
   createGrid(gridSize);
+}
+function handleResizeBtn() {
+  const userSize = prompt("Choose a new size between 1 and 100 squares");
+  removeGrid();
+  createGrid(userSize);
 }
